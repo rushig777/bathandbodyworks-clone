71bfd582bd2cd45afcb392b075452ef4670c42c7
rushig777 <gaikwadrushi777@gmail.com>
2022-05-06 18:30:38 +0530

    ..

---
+++ my-project/src/components/DropdownMenu.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import styles from "../css/dropDown.module.css";
+import Title from "../data/dropDown.json";
+export const Dropdown = () => {
+  let data = Title.Title;
+  const [sublist, setsublist] = useState([]);
+  return (
+    <div className={styles.mainDiv}>
+      {data.map((el, i) => {
+        return (
+          <>
+            <div className={styles.optionDiv}>
+              <p
+                onMouseOver={() => setsublist(el.list[0].sublist)}
+                onMouseOut={() => setsublist([])}
+                key={i}
+              >
+                {el.title}
+              </p>
+              <div className={styles.mainHovar}>
+                <div>
+                  <div>
+                    {el.list.map((el, i) => {
+                      return (
+                        <>
+                          <div
+                            onMouseOver={() => setsublist(el.sublist)}
+                            onBlur={() => setsublist([])}
+                            className={styles.listDiv}
+                          >
+                            <p>{el.listTitle}</p>
+                            {el.sublist.length > 1 ? (
+                              <img
+                                src="https://cdn-icons-png.flaticon.com/512/709/709586.png"
+                                alt=""
+                              />
+                            ) : (
+                              ""
+                            )}
+                          </div>
+                        </>
+                      );
+                    })}
+                  </div>
+                  <div className={styles.sublist}>
+                    {sublist.map((el, i) => {
+                      return <p key={i}>{el}</p>;
+                    })}
+                  </div>
+                </div>
+                <div className={styles.staticDiv}>
+                  {el.staticData.map((el, i) => {
+                    return <p key={i}>{el}</p>;
+                  })}
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      })}
+    </div>
+  );
+};
