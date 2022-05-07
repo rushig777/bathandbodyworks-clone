35354c090330d8735a735ef8a4b72568aebbde37
rushig777 <gaikwadrushi777@gmail.com>
2022-05-07 11:34:10 +0530

    ..added

---
+++ my-app/src/components/DropdownMenu.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import styles from "../css/dropDown.module.css";
+import Title from "../data/dropDown.json";
+export const Dropdown = () => {
+  let data = Title.Title;
+  const [sublist, setsublist] = useState([]);
+  return (
+    <div className={styles.mainDiv}>
+      {data.map((el, i) => {
+        return (
+            <div  key={i} className={styles.optionDiv}>
+              <p
+                 onMouseOver={() => setsublist(el.list[0].sublist)}
+                 onMouseOut={() => setsublist([])}
+              >
+                {el.title}
+              </p>
+              <div className={styles.mainHovar}>
+                <div>
+                <div>
+                  <div>
+                    {el.list.map((el, i) => {
+                      return (
+                        
+                          <div key ={i}
+                            onMouseOver={() => setsublist(el.sublist)}
+                            onBlur={() => setsublist([])}
+                            className={styles.listDiv}
+                          >
+                            <p>{el.listTitle}</p>
+                            {el.sublist.length > 0 ? (
+                              <img
+                                src="https://cdn-icons-png.flaticon.com/512/709/709586.png"
+                                alt=""
+                              />
+                            ) : (
+                              ""
+                            )}
+                          </div>
+                      );
+                    })}
+                  </div>
+                  <div className={styles.sublist}>
+                    {sublist.map((el, i) => {
+                      return <p key={i}>{el}</p>;
+                    })}
+                  </div>
+                </div>
+                <div className={styles.staticDiv}>
+                  {el.staticData.map((el, i) => {
+                    return <div key={i}>{el}</div>;
+                  })}
+                </div>
+                </div>
+              </div>
+            </div>
+        );
+      })}
+    </div>
+  );
+};
---
+++ my-app/src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import styles from "../css/homepage.module.css";
+export const Homepage = () => {
+  return (
+    <div>
+      <p className={styles.itemDiv}>Home</p>
+      <div className={styles.hoverDiv}>
+        hello you have sucessfully done this
+      </div>
+    </div>
+  );
+};
