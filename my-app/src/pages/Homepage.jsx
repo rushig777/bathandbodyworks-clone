fd46a5a19fa5ed284b30c57a6759545e250ae34e
rushig777 <gaikwadrushi777@gmail.com>
2022-05-07 12:16:47 +0530

    ....

--- my-app/src/pages/Homepage.jsx
+++ my-app/src/pages/Homepage.jsx
@@ -1,11 +1,16 @@
-import styles from "../css/homepage.module.css";
+
+import Slider from '../components/swiper'
+import Image1 from '../components/Image1'
+import Imagestwo from '../components/imagestwo'
+import Shop_Category from '../components/Shop_Category'
+import "../css/homepage.css"
 export const Homepage = () => {
   return (
     <div>
-      <p className={styles.itemDiv}>Home</p>
-      <div className={styles.hoverDiv}>
-        hello you have sucessfully done this
-      </div>
+     <Image1/>
+     <Slider/>
+     <Imagestwo/>
+     <Shop_Category/>
     </div>
   );
 };
