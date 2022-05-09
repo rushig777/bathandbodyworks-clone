c92816b74e265ef890c337a8a5a61cf7eb5ae837
rushig777 <gaikwadrushi777@gmail.com>
2022-05-09 14:48:46 +0530

    ..99%

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,10 +11,6 @@ function App() {
   return (
     <div className="App">
       <SubRoutes/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
       <Navbar/>
       <MainRoutes/>
       <Footer/>
